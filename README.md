0252b81fe78742a32e592c6c50eaf99eb94a163b
Denny Christochowitz <denny.christochowitz@gmail.com>
2019-03-01 16:47:26 +0100

    make npm package

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
+# @dchowitz/create-nodejs-project
+
+![npm (scoped)](https://img.shields.io/npm/v/@dchowitz/create-nodejs-project.svg)
+
 Creates a NodeJs barebone project with eslint, prettier and standard style.
+
+## Install
+
+```bash
+npm install -g @dchowitz/create-nodejs-project
+```
+
+## Usage
 
 ```bash
 mkdir awesome-project
 cd awesome-project
-<create-nodejs-project-folder>/init.sh
+create-nodejs-project
 ```
--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# prerequisites:
-# `mkdir project`
-# `cd project`
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# from: https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 npm init -y
 npm install --save-dev eslint eslint-config-standard eslint-plugin-import eslint-plugin-jest eslint-plugin-node eslint-plugin-promise eslint-plugin-standard jest prettier-eslint
